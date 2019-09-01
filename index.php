4579e3f0fe15f6359f2df6b35463d124d003efec
Rafael Paiva <raffaelgustavo@hotmail.com>
2019-09-01 19:11:50 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,6 +22,8 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
@@ -35,6 +39,23 @@ $app->get('/admin/login', function() {
 
 	$page->setTpl("login");
 
+});
+
+$app->post('/admin/login', function(){ //metodo para enviar o formulario preenchido na pagina login
+
+	User::login($_POST["login"], $_POST["password"]); 
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -3,20 +3,71 @@
 namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
+use \Hcode\Model;
 
-class User {
+class User extends Model {
 
-	public static function login($login, $password){
+		const SESSION = "User";
 
+	public static function login ($login, $password){
 
 		$sql = new Sql();
 
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			":LOGIN"=>$login //select no banco e verifica se encontra o login
+		));
 
+		if (count($results) === 0) // se não encontrar o login estoura um erro
+		{
+			throw new \Exception("Usuário inexistente ou senha inválida");
+			
+		}
 
+		$data = $results[0]; // a linha encontrada atribui a data
+
+		if (password_verify($password, $data["despassword"]) === true) //faz verificação do password digitado com o hash encontrado na chave despassword no banco, se for igual cai dentro do if
+		{
+			$user = new User(); // criar um objeto com os dados do usuario
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user; // retorna o usuarios localizado no banco
+
+		} else {
+
+			throw new \Exception("Usuário inexistente ou senha inválida");
+
+		}
 
 	}
 
+	public static function verifyLogin($inadmin = true)
+	{
 
+		if (
+			!isset($_SESSION[User::SESSION]) //verifica se a sessão foi definida, não sendo cai no header
+			||
+			!$_SESSION[User::SESSION] // não contem dados
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0 //se o id não foi maior que zero
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin //se o usuario do banco não tem permisão no campo inadmin, caso n tenha o inadmin retorna false
+		) {
+
+			header("Location: /admin/login");
+			exit;
+		}
+
+	}
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+		
+	}
 
 
 }
