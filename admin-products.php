45dc9adefe3d9104f2ce6392607018087a7f492c
Rafael Paiva <raffaelgustavo@hotmail.com>
2019-09-14 11:36:58 -0300

    adicionado admin produtos

--- admin-products.php
+++ admin-products.php
@@ -79,6 +79,22 @@ $app->post('/admin/products/:idproduct', function($idproduct) {
 
 });
 
+$app->get('/admin/products/:idproduct/delete', function($idproduct) {
+    
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$product->delete();
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+
 
 
 
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -55,22 +55,72 @@ class Product extends Model {
 		));
 	}
 
+	public function checkPhoto()
+	{
+		if (file_exists(
+			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR .
+			"site" . DIRECTORY_SEPARATOR .
+			"img". DIRECTORY_SEPARATOR .
+			"products" . DIRECTORY_SEPARATOR .
+			$this->getidproduct() . ".jpg"
+			)) {
 
-	public function setPhoto($file){
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
 
-		$dir = "res/site/img/products/";
+		} else {
 
-		$file = implode(",", $file);
+			$url = "/res/site/img/product.jpg"; 
 
-		$photo = $dir.$file;
+		}
 
-		$sql = new Sql();
+		return $this->setdesphoto($url);
+	}
 
-		$sql->query("INSERT INTO tb_products (desphoto) VALUES :desphoto", array(
-			":desphoto"=>$photo
+	public function getValues()
+	{
+		$this->checkPhoto();
 
-		));
+		$values = parent::getValues();
 
+		return $values;
+
+	}
+
+	public function setPhoto($file)
+	{
+
+		$extension = explode(".", $file['name']);
+		$extension = end($extension);
+
+		switch ($extension) {
+			
+			case "jpg":
+			case "jpeg":
+			$image = imagecreatefromjpeg($file['tmp_name']);
+			break;
+
+			case "gif":
+			$image = imagecreatefromgif($file['tmp_name']);
+			break;
+
+			case "png":
+			$image = imagecreatefrompng($file['tmp_name']);
+			break;
+		}
+
+		$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR .
+			"site" . DIRECTORY_SEPARATOR .
+			"img". DIRECTORY_SEPARATOR .
+			"products" . DIRECTORY_SEPARATOR .
+			$this->getidproduct() . ".jpg";
+
+		imagejpeg($image, $dist);
+
+		imagedestroy($image);
+
+		$this->checkPhoto();
 	}
 
 }
